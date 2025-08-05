84c91a63b6cea9bc1c683173cc93f0cb92674fbe
Ryan Brownswell <ryanbrownswell@gmail.com>
2025-08-05 11:44:42 +0300

    Finish "2.9 Collision detection" and fix mising axis rotation lock

--- src/assets_loader.rs
+++ src/assets_loader.rs
@@ -3,9 +3,13 @@ use bevy::prelude::*;
 #[derive(Resource, Debug, Default)]
 pub struct SceneAssets {
     pub floor: Handle<Scene>,
+    pub floor_mesh: Handle<Mesh>,
     pub landing_pad: Handle<Scene>,
+    pub landing_pad_mesh: Handle<Mesh>,
     pub launch_pad: Handle<Scene>,
+    pub launch_pad_mesh: Handle<Mesh>,
     pub rocket: Handle<Scene>,
+    pub rocket_mesh: Handle<Mesh>,
 }
 
 pub struct AssetLoaderPlugin;
@@ -21,22 +25,57 @@ fn load_assets(
     mut scene_assets: ResMut<SceneAssets>,
     asset_server: Res<AssetServer>,
 ) {
+    // For proper positioning mesh shoud apply transformation in Blender (move object origin to center of world)
+    let floor_mesh: Handle<Mesh> = asset_server.load(
+        GltfAssetLabel::Primitive {
+            mesh: 0,
+            primitive: 0,
+        }
+        .from_asset("floor.glb"),
+    );
+    let launch_pad_mesh: Handle<Mesh> = asset_server.load(
+        GltfAssetLabel::Primitive {
+            mesh: 0,
+            primitive: 0,
+        }
+        .from_asset("launch_pad.glb"),
+    );
+    let landing_pad_mesh: Handle<Mesh> = asset_server.load(
+        GltfAssetLabel::Primitive {
+            mesh: 0,
+            primitive: 0,
+        }
+        .from_asset("landing_pad.glb"),
+    );
+    let rocket_mesh: Handle<Mesh> = asset_server.load(
+        GltfAssetLabel::Primitive {
+            mesh: 0,
+            primitive: 0,
+        }
+        .from_asset("rocket.glb"),
+    );
+
     // TODO: Make proper assets handling and load using one gltf file (check https://bevy.org/examples/assets/multi-asset-sync/ for example)
+    // TODO: Think about better implementation for loading meshes
 
     *scene_assets = SceneAssets {
         floor: asset_server.load(
             GltfAssetLabel::Scene(0).from_asset("floor.glb"),
         ),
+        floor_mesh,
         landing_pad: asset_server.load(
             GltfAssetLabel::Scene(0)
                 .from_asset("landing_pad.glb"),
         ),
+        landing_pad_mesh,
         launch_pad: asset_server.load(
             GltfAssetLabel::Scene(0)
                 .from_asset("launch_pad.glb"),
         ),
+        launch_pad_mesh,
         rocket: asset_server.load(
             GltfAssetLabel::Scene(0).from_asset("rocket.glb"),
         ),
+        rocket_mesh,
     };
 }
--- src/main.rs
+++ src/main.rs
@@ -25,8 +25,9 @@ fn main() {
     .add_plugins((
         DefaultPlugins,
         AssetLoaderPlugin,
-        PhysicsPlugins::default(),
         InputManagerPlugin::<Action>::default(),
+        PhysicsPlugins::default(),
+        PhysicsDebugPlugin::default(),
     ))
     .add_systems(Startup, setup)
     .add_systems(Update, on_update);
@@ -59,39 +60,61 @@ fn setup(
     mut commands: Commands,
     scene_assets: Res<SceneAssets>,
 ) {
-    commands.spawn((
-        SceneRoot(scene_assets.floor.clone()),
-        RigidBody::Static,
-        ColliderConstructorHierarchy::new(
-            ColliderConstructor::ConvexDecompositionFromMesh,
-        ),
-    ));
-    commands.spawn((
-        SceneRoot(scene_assets.landing_pad.clone()),
-        RigidBody::Static,
-        ColliderConstructorHierarchy::new(
-            ColliderConstructor::ConvexDecompositionFromMesh,
-        ),
-    ));
+    commands
+        .spawn((
+            SceneRoot(scene_assets.floor.clone()),
+            RigidBody::Static,
+            Restitution::new(0.1),
+            Mesh3d(scene_assets.floor_mesh.clone()),
+            CollisionEventsEnabled,
+            ColliderConstructor::TrimeshFromMesh,
+        ))
+        .observe(
+            |trigger: Trigger<OnCollisionStart>,
+             player_query: Query<&Player>| {
+                if player_query.contains(trigger.collider) {
+                    println!("You lost...");
+                }
+            },
+        );
+
+    commands
+        .spawn((
+            SceneRoot(scene_assets.landing_pad.clone()),
+            RigidBody::Static,
+            Restitution::new(0.1),
+            Mesh3d(scene_assets.landing_pad_mesh.clone()),
+            ColliderConstructor::ConvexHullFromMesh,
+            CollisionEventsEnabled,
+        ))
+        .observe(
+            |trigger: Trigger<OnCollisionStart>,
+             player_query: Query<&Player>| {
+                if player_query.contains(trigger.collider) {
+                    println!("You win!");
+                }
+            },
+        );
+
     commands.spawn((
         SceneRoot(scene_assets.launch_pad.clone()),
         RigidBody::Static,
-        ColliderConstructorHierarchy::new(
-            ColliderConstructor::ConvexDecompositionFromMesh,
-        ),
+        Restitution::new(0.1),
+        Mesh3d(scene_assets.launch_pad_mesh.clone()),
+        ColliderConstructor::ConvexHullFromMesh,
     ));
 
     commands.spawn((
         SceneRoot(scene_assets.rocket.clone()),
-        // Transform::from_xyz(-7.5, 1.5, 0.0),
         Transform::from_xyz(-7.5, 2.5, 0.0),
         // TODO: Split to separate plugins
         // Physics components --------------------
         RigidBody::Dynamic,
-        ColliderConstructorHierarchy::new(
-            ColliderConstructor::ConvexDecompositionFromMesh,
-        ),
-        LockedAxes::new().lock_translation_z(),
+        Restitution::new(0.1),
+        Mesh3d(scene_assets.rocket_mesh.clone()),
+        ColliderConstructor::ConvexHullFromMesh,
+        LockedAxes::new().lock_translation_z().lock_rotation_x(),
+        CollisionEventsEnabled,
         // ---------------------------------------
         InputMap::new([
             (Action::Boost, KeyCode::Space),
@@ -134,14 +157,9 @@ fn on_update(
         action_state,
     ) = &mut *player;
 
-    println!(
-        "rot: {:?} : {:?}",
-        transform.rotation.x, transform.rotation.y
-    );
-
     if action_state.pressed(&Action::Boost) {
         let top = transform.rotation * Vec3::Y;
-        let multiplier = time.delta_secs() * 20.0;
+        let multiplier = time.delta_secs() * 15.0;
 
         linear_velocity.x += top.x * multiplier;
         linear_velocity.y += top.y * multiplier;
